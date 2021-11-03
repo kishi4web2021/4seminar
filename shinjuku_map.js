304165a57e0da16cface01065f1dd76b9a91bd51
shizuuuu <65007855+shizuuuu@users.noreply.github.com>
2021-11-03 22:31:59 +0900

    Add files via upload

--- shinjuku_map.js
+++ shinjuku_map.js
@@ -1,52 +1,112 @@
+var map;
+var currentInfoWindow = null;
+
+var place_cafe = [], place_famires = [], place_hamburger = [], place_karaoke = [], place_netcafe = [];
+var markers_cafe = [], markers_famires = [], markers_hamburger = [], markers_karaoke = [], markers_netcafe = []; 
+
+function createData(results) {
+  for (let i=0; i<results.length; i++) {
+    x = results[i].geometry.location;
+    y = results[i].name;
+    z = results[i].price_level;
+    x["name"] = y;
+    x["price"] = z;
+
+    switch(results[i].shop) {
+      case "cafe":
+        place_cafe.push(x);
+        break;
+      case "famires":
+        place_famires.push(x);
+        break;
+      case "hamburger":
+        place_hamburger.push(x);
+        break;
+      case "karaoke":
+        place_karaoke.push(x);
+        break;
+      case "netcafe":
+        place_netcafe.push(x);
+        break;
+    }
+  } 
+  /*
+  place:
+  {
+    i:
+      lat: xxx
+      lng: yyy
+      name: "zzz"
+      price: 0
+  }
+  */
+}
+
+function createMarker(map, i, place, markers) {
+  var marker = new google.maps.Marker({
+    position: { lat:place[i].lat, lng:place[i].lng },
+    map: map,
+    title: place[i].name,
+  });
+
+  markers.push(marker);
+
+  var contentStr = '<a>' + place[i].name + '<br>●Wi-Fi</a>';
+
+  var infoWindow = new google.maps.InfoWindow({
+    content: contentStr,
+  });
+
+  google.maps.event.addListener(marker, 'click', function(){
+    if(currentInfoWindow != null) {
+      currentInfoWindow.close();
+    }
+    infoWindow.open(map, marker);
+    currentInfoWindow = infoWindow;
+  });
+}
+
+function cafe(map) {
+  for (var i=0; i<place_cafe.length; i++) {
+    createMarker(map, i, place_cafe, markers_cafe);
+  }
+}
+
+function famires(map) {
+  for (var i=0; i<place_famires.length; i++) {
+    createMarker(map, i, place_famires, markers_famires);
+  }
+}
+
+function hamburger(map) {
+  for (var i=0; i<place_hamburger.length; i++) {
+    createMarker(map, i, place_hamburger, markers_hamburger);
+  }
+}
+
+function karaoke(map) {
+  for (var i=0; i<place_karaoke.length; i++) {
+    createMarker(map, i, place_karaoke, markers_karaoke);
+  }
+}
+
+function netcafe(map) {
+  for (var i=0; i<place_netcafe.length; i++) {
+    createMarker(map, i, place_netcafe, markers_netcafe);
+  }
+}
+
 function initMap() {
   var target = document.getElementById('map');  
-  var latlng = {lat: 35.69092, lng: 139.7002579};  //新宿駅の緯度経度
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var latlng = { lat: 35.69092, lng: 139.7002579 };
+  map = new google.maps.Map(document.getElementById('map'), {
     center: latlng,
     zoom: 16
   });
-  
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
-
-  var request1 = {
-    location: latlng, 
-    radius: 300,  
-    keyword: 'カラオケ館ORカラオケマックORジョイサウンドORカラオケの鉄人ORカラオケパセラORカラオケバンバンOR歌広場ORワンカラ'
-  };
-
-  var request2 = {
-    location: latlng, 
-    radius: 300,  
-    keyword: '快活クラブORグランサイバーカフェバグースORマンボーORカスタマカフェORコミックバスターORネットカフェジャム'
-  };
-  
-  service.nearbySearch(request1, callback); //カラオケ
-  service.nearbySearch(request2, callback); //ネットカフェ
-
-  // カフェ
-  for (let i=0; i<shinjuku_cafe.results.length; i++) { 
-    createMarker(shinjuku_cafe.results[i]);
-  }
-  
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-    }
-  }
-  
-  function createMarker(place) {
-    //var placeLoc = place.geometry.location; 
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location  //results[i].geometry.location
-    });
- 
-    marker.addListener('click', function() {
-      infowindow.setContent(place.name);  //results[i].name
-      infowindow.open(map, this);
-    });
-  }
+  createData(shinjuku_data.results);
+  cafe(map);
+  famires(map);
+  hamburger(map); 
+  karaoke(map);
+  netcafe(map);
 }
