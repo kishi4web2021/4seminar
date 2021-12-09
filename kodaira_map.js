daaeb37e7736d69703c803aadd812a5a3856527b
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-09 13:19:01 +0900

    Update kodaira_map.js

--- kodaira_map.js
+++ kodaira_map.js
@@ -6,47 +6,47 @@ function initMap() {
     zoom: 20
   });
     
-  var infowindow = new google.maps.InfoWindow();
-  var service = new google.maps.places.PlacesService(map);
+  //var infowindow = new google.maps.InfoWindow();
+  //var service = new google.maps.places.PlacesService(map);
 
-  var request1 = {
-    location: latlng,
-    radius: 2000,
-    keyword: 'カラオケ館ORカラオケマックORジョイサウンドORカラオケの鉄人ORカラオケパセラORカラオケバンバンOR歌広場ORワンカラ'
-  };
+  //var request1 = {
+    //location: latlng,
+    //radius: 2000,
+    //keyword: 'カラオケ館ORカラオケマックORジョイサウンドORカラオケの鉄人ORカラオケパセラORカラオケバンバンOR歌広場ORワンカラ'
+  //};
 
-  var request2 = {
-    location: latlng,
-    radius: 2000,
-    keyword: '快活クラブORグランサイバーカフェバグースORマンボーORカスタマカフェORコミックバスターORネットカフェジャム'
-  };
+  //var request2 = {
+    //location: latlng,
+    //radius: 2000,
+    //keyword: '快活クラブORグランサイバーカフェバグースORマンボーORカスタマカフェORコミックバスターORネットカフェジャム'
+  //};
   
-  service.nearbySearch(request1, callback); //カラオケ
-  service.nearbySearch(request2, callback); //ネットカフェ
+  //service.nearbySearch(request1, callback); //カラオケ
+  //service.nearbySearch(request2, callback); //ネットカフェ
 
   // カフェ
-  for (let i=0; i<kodaira_cafe.results.length; i++) { 
-    createMarker(kodaira_cafe.results[i]);
-  }
+  //for (let i=0; i<kodaira_cafe.results.length; i++) { 
+    //createMarker(kodaira_cafe.results[i]);
+  //}
   
-  function callback(results, status) {
-    if (status === google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        createMarker(results[i]);
-      }
-    }
-  }
+  //function callback(results, status) {
+    //if (status === google.maps.places.PlacesServiceStatus.OK) {
+      //for (var i = 0; i < results.length; i++) {
+        //createMarker(results[i]);
+      //}
+    //}
+  //}
   
-  function createMarker(place) {
+  //function createMarker(place) {
     //var placeLoc = place.geometry.location; 
-    var marker = new google.maps.Marker({
-      map: map,
-      position: place.geometry.location  //results[i].geometry.location
-    });
+    //var marker = new google.maps.Marker({
+      //map: map,
+      //position: place.geometry.location  //results[i].geometry.location
+    //});
  
-    marker.addListener('click', function() {
-      infowindow.setContent(place.name);  //results[i].name
-      infowindow.open(map, this);
-    });
-  }
-}
+    //marker.addListener('click', function() {
+      //infowindow.setContent(place.name);  //results[i].name
+      //infowindow.open(map, this);
+    //});
+  //}
+//}
