28d279a89866cf7c96166b61d6e28aef35fa0861
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-18 22:50:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 //test use shinjuku data
 
+var map;
+var currentInfoWindow = null;
+
+var place_cafe = [], place_famires = [], place_hamburger = [], place_karaoke = [], place_netcafe = [];
+var markers_cafe = [], markers_famires = [], markers_hamburger = [], markers_karaoke = [], markers_netcafe = []; 
