f5773e19b93277ec0f82013733fbebbde46c3b96
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-21 14:53:59 +0900

    Update shinjuku_map.js

--- shinjuku_map.js
+++ shinjuku_map.js
@@ -67,44 +67,10 @@ function createMarker(i, place) {
     currentInfoWindow = infoWindow;
   });
 }
-
-// Sets the map on all markers in the array.
-function setMapOnAll(map,markers){
-  for (let i = 0; i < markers.length; i++) {
-    markers[i].setMap(map);
-  }
-}
-
-// Removes the markers from the map, but keeps them in the array.
-function hideMarkers() {
-  setMapOnAll(null);
-}
-
-// Shows any markers currently in the array.
-function showMarkers() {
-  setMapOnAll(map);
-}
-
-// Deletes all markers in the array by removing references to them.
-function deleteMarkers(markers) {
-  hideMarkers();
-  markers = [];
-}
-
 function cafe() {
-  //if(n=1){
   for (var i=0; i<place_cafe.length; i++) {
     createMarker(i, place_cafe);
   }
-  //setMapOnAll(map,markers_cafe);
-  
-    //showMarkers();
-  //}else{
-    //for (let i = 0; i < markers_cafe.length; i++) {
-      //markers_cafe[i].setMap(null);
-    //}
-    //markers_cafe = [];
-  //}
 }
 
 function famires() {
@@ -132,16 +98,13 @@ function netcafe() {
 }
 
 function initMap() {
-  var target = document.getElementById('map');  
-  var latlng = { lat: 35.69092, lng: 139.7002579 };
-  map = new google.maps.Map(document.getElementById('map'), {
+  var latlng = new google.maps.LatLng(35.69092, 139.7002579);
+  var opts = {
+    zoom: 16,
     center: latlng,
-    zoom: 16
-  });
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+  
   createData(shinjuku_data.results);
-  //cafe();
-  //famires();
-  //hamburger(); 
-  //karaoke();
-  //netcafe();
 }
