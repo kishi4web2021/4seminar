84d047a7526aa4d65eef1365176a66a5ab73f3aa
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-23 08:24:40 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,48 @@
-//test use shinjuku data
-
 var map;
-var currentInfoWindow = null;
+var marker;
 
-var place_cafe = [], place_famires = [], place_hamburger = [], place_karaoke = [], place_netcafe = [];
-var markers_cafe = [], markers_famires = [], markers_hamburger = [], markers_karaoke = [], markers_netcafe = []; 
+var place_cafe = [];
+var markers_cafe = [];
+
+function initMap() {
+  var latlng = new google.maps.LatLng(35.69092, 139.7002579);
+  var opts = {
+    zoom: 16,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+  
+  createData(shinjuku_data.results);
+  
+  var m_latlng = new google.maps.LatLng(place_cafe[1].lat,place_cafe[1].lng);
+  marker = new google.maps.Marker({
+    position: m_latlng,
+  });
+}
+
+function createData(results) {
+  for (let i=0; i<results.length; i++) {
+    x = results[i].geometry.location;
+    y = results[i].name;
+    z = results[i].price_level;
+    s = results[i].url
+    x["name"] = y;
+    x["price"] = z;
+    x["url"] = s;
+
+    switch(results[i].shop) {
+      case "cafe":
+        place_cafe.push(x);
+        break;
+    }
+  } 
+}
+
+function doOpen() {
+  marker.setMap(map);
+}
+
+function doClose() {
+  marker.setMap(null);
+}
