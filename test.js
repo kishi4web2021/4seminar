0f17b0119fd592907b0acc6e44b08eaf7fa6d18f
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-24 02:59:19 +0900

    Update test.js

--- test.js
+++ test.js
@@ -19,7 +19,7 @@ function initMap() {
   //marker = new google.maps.Marker({
     //position: m_latlng,
   //});
-  createcafemarker();
+  createmarker();
 }
 
 function createData(results) {
@@ -52,12 +52,40 @@ function createData(results) {
   } 
 }
 
-function createcafemarker(){
-  var m_latlng = [];
+function createmarker(){
+  var cafe_latlng = [];
   for(var i=0; i<place_cafe.length; i++){
-    m_latlng[i] = new google.maps.LatLng(place_cafe[i].lat,place_cafe[i].lng);
+    cafe_latlng[i] = new google.maps.LatLng(place_cafe[i].lat,place_cafe[i].lng);
     markers_cafe[i] = new google.maps.Marker({
-      position: m_latlng[i],
+      position: cafe_latlng[i],
+    });
+  }
+  var famires_latlng = [];
+  for(var j=0; j<place_famires.length; j++){
+    famires_latlng[j] = new google.maps.LatLng(place_famires[j].lat,place_famires[j].lng);
+    markers_famires[j] = new google.maps.Marker({
+      position: famires_latlng[j],
+    });
+  }
+  var hamburger_latlng = [];
+  for(var k=0; k<place_hamburger.length; k++){
+    hamburger_latlng[k] = new google.maps.LatLng(place_hamburger[k].lat,place_hamburger[k].lng);
+    markers_hamburger[k] = new google.maps.Marker({
+      position: hamburger_latlng[k],
+    });
+  }
+  var karaoke_latlng = [];
+  for(var m=0; m<place_karaoke.length; m++){
+    karaoke_latlng[m] = new google.maps.LatLng(place_karaoke[m].lat,place_karaoke[m].lng);
+    markers_karaoke[m] = new google.maps.Marker({
+      position: karaoke_latlng[m],
+    });
+  }
+  var netcafe_latlng = [];
+  for(var n=0; n<place_netcafe.length; n++){
+    netcafe_latlng[n] = new google.maps.LatLng(place_netcafe[n].lat,place_netcafe[n].lng);
+    markers_netcafe[n] = new google.maps.Marker({
+      position: netcafe_latlng[n],
     });
   }
 }
