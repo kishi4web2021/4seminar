8deaf469b017fe62427c33d11b6b75cf3dee5a7d
haru99ka <90190969+haru99ka@users.noreply.github.com>
2021-12-23 12:27:39 +0900

    Update shinjuku_map.js

--- shinjuku_map.js
+++ shinjuku_map.js
@@ -40,20 +40,26 @@ function createData(results) {
       lng: yyy
       name: "zzz"
       price: 0
+      url: "https://..."
   }
   */
 }
 
-function createMarker(i, place) {
+function createMarker(i, place, icon) {
   var marker = new google.maps.Marker({
     position: { lat:place[i].lat, lng:place[i].lng },
     map: map,
     title: place[i].name,
+    icon: {
+	url: icon ,
+	scaledSize: new google.maps.Size( 27, 40 ) ,
+  }
+
   });
 
   //markers.push(marker);
 
-  var contentStr = '<a>' + place[i].name + '<br>●Wi-Fi</a><br><a href=' + place[i].url + '>Google検索</a>';
+  var contentStr = '<a>' + place[i].name + '<br>●Wi-Fi</a>' + '<br><a href=' + place[i].url + '>Google検索</a>';
 
   var infoWindow = new google.maps.InfoWindow({
     content: contentStr,
@@ -68,48 +74,85 @@ function createMarker(i, place) {
   });
 }
 
-function cafe() {
-  var cafe_latlng = [];
-  for (var i=0; i<place_cafe.length; i++) {
-    cafe_latlng[i] = new google.maps.LatLng(place_cafe[i].lat,place_cafe[i].lng);
-    markers_cafe[i] = new google.maps.Marker({
-      position: cafe_latlng[i]
-    });
+// Sets the map on all markers in the array.
+function setMapOnAll(map,markers){
+  for (let i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
   }
 }
 
+// Removes the markers from the map, but keeps them in the array.
+function hideMarkers() {
+  setMapOnAll(null,null);
+}
+
+// Shows any markers currently in the array.
+function showMarkers() {
+  setMapOnAll(map);
+}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers(markers) {
+  hideMarkers();
+  markers = [];
+}
+
+function cafe() {
+  icon = 'img/icon1.png'
+  //if(n=1){
+  for (var i=0; i<place_cafe.length; i++) {
+      createMarker(i, place_cafe, icon);
+  }
+  //setMapOnAll(map,markers_cafe);
+  
+    //showMarkers();
+  //}else{
+    //for (let i = 0; i < markers_cafe.length; i++) {
+      //markers_cafe[i].setMap(null);
+    //}
+    //markers_cafe = [];
+  //}
+}
+
 function famires() {
+  icon = 'img/icon2.png'
   for (var i=0; i<place_famires.length; i++) {
-    createMarker(i, place_famires, markers_famires);
+      createMarker(i, place_famires, icon);
   }
 }
 
 function hamburger() {
+  icon = 'img/icon3.png'
   for (var i=0; i<place_hamburger.length; i++) {
-    createMarker(i, place_hamburger, markers_hamburger);
+      createMarker(i, place_hamburger, icon);
   }
 }
 
 function karaoke() {
+  icon = 'img/icon4.png'
   for (var i=0; i<place_karaoke.length; i++) {
-    createMarker(i, place_karaoke, markers_karaoke);
+      createMarker(i, place_karaoke, icon);
   }
 }
 
 function netcafe() {
+  icon = 'img/icon5.png'
   for (var i=0; i<place_netcafe.length; i++) {
-    createMarker(i, place_netcafe, markers_netcafe);
+      createMarker(i, place_netcafe, icon);
   }
 }
 
 function initMap() {
-  var latlng = new google.maps.LatLng(35.69092, 139.7002579);
-  var opts = {
-    zoom: 16,
+  var target = document.getElementById('map');  
+  var latlng = { lat: 35.69092, lng: 139.7002579 };
+  map = new google.maps.Map(document.getElementById('map'), {
     center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("map_canvas"), opts);
-  
+    zoom: 16
+  });
   createData(shinjuku_data.results);
+  //cafe();
+  famires();
+  hamburger(); 
+  karaoke();
+  netcafe();
 }
