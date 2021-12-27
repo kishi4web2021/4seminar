421686339dcaf307dbc42457c07b00310a7e4f5a
shibuya <65008051+Haruo-es@users.noreply.github.com>
2021-12-28 07:14:15 +0900

    Update kodaira_map.js

--- kodaira_map.js
+++ kodaira_map.js
@@ -1,3 +1,146 @@
+var map;
+
+var place_cafe = [], place_famires = [], place_hamburger = [], place_karaoke = [], place_netcafe = [];
+var markers_cafe = [], markers_famires = [], markers_hamburger = [], markers_karaoke = [], markers_netcafe = [];
+
+function initMap() {
+  var latlng = new google.maps.LatLng(35.7217636, 139.4667473);
+  var opts = {
+    zoom: 14,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), opts);
+  
+  createData(shinjuku_data.results);
+  
+  createmarker();
+}
+
+function createData(results) {
+  for (let i=0; i<results.length; i++) {
+    x = results[i].geometry.location;
+    y = results[i].name;
+    z = results[i].price_level;
+    s = results[i].url
+    x["name"] = y;
+    x["price"] = z;
+    x["url"] = s;
+
+    switch(results[i].shop) {
+      case "cafe":
+        place_cafe.push(x);
+        break;
+      case "famires":
+        place_famires.push(x);
+        break;
+      case "hamburger":
+        place_hamburger.push(x);
+        break;
+      case "karaoke":
+        place_karaoke.push(x);
+        break;
+      case "netcafe":
+        place_netcafe.push(x);
+        break;
+    }
+  } 
+}
+
+function createmarker(){
+  var cafe_latlng = [];
+  for(var i=0; i<place_cafe.length; i++){
+    cafe_latlng[i] = new google.maps.LatLng(place_cafe[i].lat,place_cafe[i].lng);
+    markers_cafe[i] = new google.maps.Marker({
+      position: cafe_latlng[i],
+    });
+    markers_cafe[i].addListener('click', function() {
+      infowindow.setContent(place_cafe[i].name);
+      infowindow.open(map, this);
+    });
+  }
+  var famires_latlng = [];
+  for(var j=0; j<place_famires.length; j++){
+    famires_latlng[j] = new google.maps.LatLng(place_famires[j].lat,place_famires[j].lng);
+    markers_famires[j] = new google.maps.Marker({
+      position: famires_latlng[j],
+    });
+    markers_famires[j].addListener('click', function() {
+      infowindow.setContent(place_famires[j].name);  //results[i].name
+      infowindow.open(map, this);
+    });
+  }
+  var hamburger_latlng = [];
+  for(var k=0; k<place_hamburger.length; k++){
+    hamburger_latlng[k] = new google.maps.LatLng(place_hamburger[k].lat,place_hamburger[k].lng);
+    markers_hamburger[k] = new google.maps.Marker({
+      position: hamburger_latlng[k],
+    });
+  }
+  var karaoke_latlng = [];
+  for(var m=0; m<place_karaoke.length; m++){
+    karaoke_latlng[m] = new google.maps.LatLng(place_karaoke[m].lat,place_karaoke[m].lng);
+    markers_karaoke[m] = new google.maps.Marker({
+      position: karaoke_latlng[m],
+    });
+  }
+  var netcafe_latlng = [];
+  for(var n=0; n<place_netcafe.length; n++){
+    netcafe_latlng[n] = new google.maps.LatLng(place_netcafe[n].lat,place_netcafe[n].lng);
+    markers_netcafe[n] = new google.maps.Marker({
+      position: netcafe_latlng[n],
+    });
+  }
+}
+
+function cafe(){
+  for(var i=0; i<markers_cafe.length; i++){
+    markers_cafe[i].setMap(map);
+  }
+}
+
+function famires(){
+  for(var i=0; i<markers_famires.length; i++){
+    markers_famires[i].setMap(map);
+  }
+}
+
+function hamburger(){
+  for(var i=0; i<markers_hamburger.length; i++){
+    markers_hamburger[i].setMap(map);
+  }
+}
+
+function karaoke(){
+  for(var i=0; i<markers_karaoke.length; i++){
+    markers_karaoke[i].setMap(map);
+  }
+}
+
+function netcafe(){
+  for(var i=0; i<markers_netcafe.length; i++){
+    markers_netcafe[i].setMap(map);
+  }
+}
+
+function deletemarkersall(){
+  for(var i=0; i<markers_cafe.length; i++){
+    markers_cafe[i].setMap(null);
+  }
+  for(var j=0; j<markers_famires.length; j++){
+    markers_famires[j].setMap(null);
+  }
+  for(var k=0; k<markers_hamburger.length; k++){
+    markers_hamburger[k].setMap(null);
+  }
+  for(var l=0; l<markers_karaoke.length; l++){
+    markers_karaoke[l].setMap(null);
+  }
+  for(var m=0; m<markers_netcafe.length; m++){
+    markers_netcafe[m].setMap(null);
+  }
+}
+//
 function initMap() {
   var target = document.getElementById('map');  
   var latlng = {lat: 35.7217636, lng: 139.4667473};  //小平キャンパスの緯度経度
